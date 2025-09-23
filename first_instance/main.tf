d66618ef03d811e3e93ff0061aea72b73aa0ca54
bfrerot <benoitfrerot13@gmail.com>
2025-09-23 14:37:37 +0200

    23092025-6

---
+++ first_instance/main.tf
@@ -0,0 +1,17 @@
+resource "google_compute_instance" "movingcastle-001" {
+name = "test"
+machine_type = "e2-micro"
+zone = "europe-west9-a"
+project = "carbon-gecko-472110-u1"
+
+boot_disk {
+    initialize_params {
+        image = "debian-13"
+    }
+  }
+
+network_interface {
+    network = "default"
+  }
+}
+
---
+++ first_instance/provider.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 7.0.0"
+    }
+  }
+}
+
+provider "google" {
+  project     = "carbon-gecko-472110-u1"
+  region      = "europe-west9"
+  zone        = "europe-west9-a"
+  credentials = file(var.path)
+}
